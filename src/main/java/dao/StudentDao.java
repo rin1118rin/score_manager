93f4bb8b07b35f51491592dd24916dbbba338634
rin1118rin <fko2547047@stu.o-hara.ac.jp>
2026-04-21 14:59:45 +0900

    あとでえらーきえる

--- src/main/java/dao/StudentDao.java
+++ src/main/java/dao/StudentDao.java
@@ -1,5 +1,28 @@
 package dao;
 
-public class StudentDao {
-
+public class StudentDao extends Dao {
+	
+	private String baseSql;
+	
+	public Student get(String no) throws Exception {
+		
+	}
+	
+	private List<Student> postFilter(ResultSet rSet, School school) throws Exception {
+		
+	}
+	
+	public List<Student> filter(School school, int entYear, String classNum, boolean isAttend) throws Exception {
+		
+	}
+	
+	public List<Student> filter(School school, int entYear, boolean isAttend) throws Exception {
+		
+	}
+	
+	public List<Student> filter(School school, boolean isAttend) throws Exception {
+		
+	}
+	
+	public boolean save(Student student) throws Exception {
 }
