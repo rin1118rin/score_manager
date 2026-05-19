e645da8620e2db0dc40591d8d1e0a426c9d611a0
hfu3i2 <fko2547034@stu.o-hara.ac.jp>
2026-05-19 15:10:58 +0900

    kkoojj

---
+++ src/main/java/dao/SubjectDao.java
@@ -0,0 +1,24 @@
+package dao;
+
+import java.util.List;
+
+import bean.School;
+import bean.Subject;
+
+public class SubjectDao extends Dao {
+	public Subject get(String cd,School school) throws Exception {
+		
+	}
+	
+	public List<Subject> filter(School school) throws Exception {
+		
+	}
+	
+	public boolean sava(Subject subject) throws Exception {
+		
+	}
+	
+	public boolean delete(Subject subject) throws Exception {
+		
+	}
+}
