ec1a27a823f66ba810d411a7c16f8e2b1216884b
hfu3i2 <fko2547034@stu.o-hara.ac.jp>
2026-06-02 11:31:48 +0900

    jini

---
+++ src/main/java/dao/UserDao.java
@@ -0,0 +1,13 @@
+package dao;
+
+public class UserDao extends Dao {
+	private boolean isAuthenticated;
+	
+	public boolean isAuthenticated() {
+		
+	}
+	
+	public void setAuthenticated(boolean isAuthenticated) {
+		
+	}
+}
